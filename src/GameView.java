2ad6a46960579042e3bce076e691d303f312bbbe
baptist3 <186568477+baptist3-ng@users.noreply.github.com>
2025-10-28 15:23:13 +0100

    Ajout fonction drawPiste, Classe Coordonnee pour les points de la piste

--- src/GameView.java
+++ src/GameView.java
@@ -1,5 +1,6 @@
 import javax.swing.JComponent;
 import java.awt.Graphics;
+import java.util.ArrayList;
 import java.awt.Color;
 import javax.swing.JFrame;
 
@@ -7,9 +8,11 @@ public class GameView extends JComponent /*implements GameObservateur*/{
     private int cellSize = 50;
     final int NB_CASES_X = 20;
     final int NB_CASES_Y = 10;
+    private GameObservable gameObservable;
 
-    public GameView(/*GameObservable gameObservable*/){
-        //gameObservable.addObserver(this); // Je m'ajoute
+    public GameView(GameObservable gameObservable){
+        gameObservable.addObserver(this); // Je m'ajoute
+        this.gameObservable = gameObservable;
         launchGame();
     }
 
@@ -22,14 +25,17 @@ public class GameView extends JComponent /*implements GameObservateur*/{
         
     }
 
+
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.GREEN);
-        g.fillRect(0, 0, cellSize * NB_CASES_X, cellSize * NB_CASES_Y);
+        g.fillRect(0, 0, cellSize * NB_CASES_X, cellSize * NB_CASES_Y);        
+        drawPiste(this.gameObservable.getPiste(), g);
         g.setColor(Color.BLACK);
         drawGrid(g, cellSize * NB_CASES_X, cellSize * NB_CASES_Y);
     }
+
 
     private void drawGrid(Graphics g, int x, int y){
         for(int i = 0; i < (x / cellSize); i++){
@@ -40,6 +46,18 @@ public class GameView extends JComponent /*implements GameObservateur*/{
         }
     }
 
-    // Fonction drawPiste()
 
+    //Fonction drawPiste(). Je récupère via GameObservable la piste.
+    private void drawPiste(ArrayList<Coordonnee> piste, Graphics g){
+        for (Coordonnee c : piste) {
+            g.setColor(Color.GRAY);
+            g.fillRect((c.getX()-1) * cellSize, (c.getY()-1) * cellSize, cellSize, cellSize);
+            // -1 parce qu'on commence à compter à 1, pas 0
+            if(c.getVirage()){
+                g.setColor(Color.YELLOW);
+                g.fillRect((c.getX()-1) * cellSize + 5, (c.getY()-1) * cellSize + 5, cellSize - 10, cellSize - 10);
+                //g.drawString("3", c.getX() * cellSize , c.getY() * cellSize);
+            }
+        }
+    }
 }
