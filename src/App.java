1fb98e331f5a85bdf2b6d75360b35e89131e79c9
GoldenKazoo <goldenkazoomail@gmail.com>
2025-12-19 15:25:47 +0100

    fixed imbriqued loop

--- src/App.java
+++ src/App.java
@@ -2,65 +2,50 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         int nbVoitures = 0;
         ArrayList<Integer> data = new ArrayList<>();
 
-        while (nbVoitures < 1 || nbVoitures > 3)
-        {
+        while (nbVoitures < 1 || nbVoitures > 3) {
             String input = JOptionPane.showInputDialog(
-                null, 
-                "Entrez le nombre de voitures (1 à 3) :", 
-                "Configuration du jeu", 
-                JOptionPane.QUESTION_MESSAGE
-            );
-            if (input == null)
-            {
+                    null,
+                    "Entrez le nombre de voitures (1 à 3) :",
+                    "Configuration du jeu",
+                    JOptionPane.QUESTION_MESSAGE);
+            if (input == null) {
                 return;
             }
             nbVoitures = Integer.parseInt(input);
-
-            
-
-            for(int i = 0; i < nbVoitures; i++){
-                String in = null;
-                while(in == null ||Integer.parseInt(in) < 1 || Integer.parseInt(in) > 4){
-                    in = JOptionPane.showInputDialog(
-                        null, 
-                        "1. BoostSound  2. Drunk  3. Hybride  4. Nothing ", 
-                        "Choisir un Decorator", 
-                        JOptionPane.QUESTION_MESSAGE
-                    );
-                }
-                data.add(Integer.parseInt(in));
-            }
         }
-        
+
+        for (int i = 0; i < nbVoitures; i++) {
+            String in = null;
+            while (in == null || Integer.parseInt(in) < 1 || Integer.parseInt(in) > 4) {
+                in = JOptionPane.showInputDialog(
+                        null,
+                        "1. BoostSound  2. Drunk  3. Hybride  4. Nothing ",
+                        "Choisir un Decorator",
+                        JOptionPane.QUESTION_MESSAGE);
+            }
+            data.add(Integer.parseInt(in));
+        }
 
         GameObservable game = new GameObservable(nbVoitures, data);
         new GameView(game);
         new Classement(game);
-        
-        for (IVoiture v : game.get_voitures())
-        {
+
+        for (IVoiture v : game.get_voitures()) {
             new DashboardView(game, v);
         }
 
-        while (!game.is_finished())
-        {
-            if (!game.is_paused())
-            {
+        while (!game.is_finished()) {
+            if (!game.is_paused()) {
                 game.moove_car();
             }
-            try
-            {
+            try {
                 Thread.sleep(500);
-            }
-            catch (InterruptedException e) 
-            {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
